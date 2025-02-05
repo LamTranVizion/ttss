39606061ac30b4a0ed4ac3fa5f44dca60ec2ba14
lamtran-vizion <lam.tran@vizion.space>
2025-02-05 14:50:58 +0700

    again

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TTS',
   plugins: [react()],
+  base: '/ttss/',
   build: {
     rollupOptions:{
       external: ['@gradio/client'],
